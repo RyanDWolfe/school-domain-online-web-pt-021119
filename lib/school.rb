8becb8a99030926ff405af812054a14557f0e089
Ryan Wolfe <ryan@studiohopfitness.com>
2019-02-27 19:18:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+
+  attr_accessor :roster
+  attr_reader :name
+
+  roster = {}
+
+  def initialize(name)
+    @name = name
+    end
+
+  # def roster
+  #   roster
+  # end
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    roster << {grade =>[name]}
+  end
+
+  def grade
+
+  end
+
+end
